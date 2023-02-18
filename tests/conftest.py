7f9a0921f0d805bd1d2bbe28ee728d007b188452
José Pablo Chaves <jpablochaves@gmail.com>
2023-02-17 19:11:05 -0600

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@
 The tests/conftest.py file contains setup functions called fixtures that each test will use. Tests are in Python modules that start with test_, and each test function in those modules also starts with test_.    
 """
 import pytest
-from calculator.models.models import (RequestOperation, ResponseOperation)
+#from calculator.models.models import (RequestOperation, ResponseOperation)
 from typing import Type
 from calculator import flask_app as app
 
@@ -12,14 +12,14 @@ def client():
     return app.test_client()
 
 
-@pytest.fixture
-def request_model() -> Type[RequestOperation]:
-    query = RequestOperation(a=4,b=2)
-    return query
+#@pytest.fixture
+#def request_model() -> Type[RequestOperation]:
+#    query = RequestOperation(a=4,b=2)
+#    return query
 
-@pytest.fixture
-def response_model() -> ResponseOperation:
-    return ResponseOperation(result=10.0)
+#@pytest.fixture
+#def response_model() -> ResponseOperation:
+#    return ResponseOperation(result=10.0)
 
 
 @pytest.fixture
